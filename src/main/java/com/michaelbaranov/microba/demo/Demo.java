679287d10af071b75880e00092aff411af1488f9
Sergey Savenko <deadok22@gmail.com>
2014-10-31 14:33:16 +0300

    drop GradientBar, GradientEditor, MarkerBar, and BirdView

--- src/main/java/com/michaelbaranov/microba/demo/Demo.java
+++ src/main/java/com/michaelbaranov/microba/demo/Demo.java
@@ -1,10 +1,13 @@
 package com.michaelbaranov.microba.demo;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import com.michaelbaranov.microba.calendar.CalendarPane;
+import com.michaelbaranov.microba.calendar.DatePicker;
+import com.michaelbaranov.microba.calendar.DatePickerCellEditor;
+import com.michaelbaranov.microba.calendar.HolidayPolicy;
+import com.michaelbaranov.microba.common.AbstractPolicy;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyVetoException;
@@ -14,40 +17,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.swing.JApplet;
-import javax.swing.JColorChooser;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTabbedPane;
-import javax.swing.JTable;
-import javax.swing.SwingConstants;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-
-import com.michaelbaranov.microba.calendar.CalendarPane;
-import com.michaelbaranov.microba.calendar.DatePicker;
-import com.michaelbaranov.microba.calendar.DatePickerCellEditor;
-import com.michaelbaranov.microba.calendar.HolidayPolicy;
-import com.michaelbaranov.microba.common.AbstractPolicy;
-import com.michaelbaranov.microba.common.BoundedTableModel;
-import com.michaelbaranov.microba.common.PolicyListener;
-import com.michaelbaranov.microba.gradient.GradientBar;
-import com.michaelbaranov.microba.gradienteditor.GradientEditor;
-import com.michaelbaranov.microba.marker.MarkerBar;
-
 public class Demo extends JApplet {
-
-	static MarkerBar bar;
-
-	static BoundedTableModel model;
-
-	private static JFrame frame;
-
-	private static JPanel panel;
 
 	public static void main(String[] s) {
 
@@ -57,38 +27,19 @@ public class Demo extends JApplet {
 	}
 
 	private void run() {
-		frame = new JFrame();
+		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout());
 
 		JTabbedPane tabs = new JTabbedPane();
 		tabs.addTab("CalendarPane", buildCalendarPaneTab());
-		 tabs.addTab("Gradient", buildGradientTab());
 		 tabs.addTab("CellEditor", buildCellEditorTab());
 		 tabs.addTab("DatePicker", buildDatePickerTab());
 
 		frame.getContentPane().add(tabs, BorderLayout.CENTER);
 		frame.setSize(500, 300);
 		frame.setVisible(true);
-
 	}
-
-	//
-	// public void init() {
-	// super.init();
-	// System.out.println("init");
-	// panel = new JPanel();
-	// panel.setLayout(new BorderLayout());
-	//
-	// JTabbedPane tabs = new JTabbedPane();
-	// tabs.addTab("CalendarPane", buildCalendarPaneTab());
-	// tabs.addTab("Gradient", buildGradientTab());
-	// tabs.addTab("CellEditor", buildCellEditorTab());
-	// tabs.addTab("DatePicker", buildDatePickerTab());
-	//
-	// panel.add(tabs, BorderLayout.CENTER);
-	// this.setContentPane(panel);
-	// }
 
 	public void start() {
 		System.out.println("start");
@@ -169,12 +120,12 @@ public class Demo extends JApplet {
 		// datePicker.showButtonOnly(false);
 		// datePicker.setToolTipText("hello!!!!");
 		// datePicker.setShowNumberOfWeek(true);
-		
+
 		Map ov = new HashMap();
 
 		ov.put(CalendarPane.COLOR_CALENDAR_GRID_FOREGROUND_ENABLED,
 				Color.ORANGE);
-		
+
 		datePicker.setColorOverrideMap(ov);
 
 		panel.setLayout(new GridBagLayout());
@@ -216,74 +167,4 @@ public class Demo extends JApplet {
 		return panel;
 
 	}
-
-	private JComponent buildGradientTab() {
-		JPanel panel = new JPanel();
-		GradientBar gradient;
-
-		GradientEditor editor;
-		JColorChooser colorChooser;
-
-		colorChooser = new JColorChooser();
-		editor = new GradientEditor();
-
-		// editor.setOrientation(SwingConstants.VERTICAL);
-
-		ColorAdaptor adaptor = new ColorAdaptor(editor, colorChooser);
-
-		panel.add(editor, BorderLayout.NORTH);
-		panel.add(colorChooser, BorderLayout.CENTER);
-
-		MarkerBar bar = new MarkerBar();
-		bar.setDataModel(editor.getDataModel());
-		bar.setPositionColumn(editor.getColorPositionColumn());
-		bar.setColorColumn(editor.getColorColumn());
-		bar.setFliped(true);
-		bar.setOrientation(SwingConstants.VERTICAL);
-		panel.add(bar, BorderLayout.EAST);
-		return panel;
-	}
-
-	private static class ColorAdaptor implements ChangeListener,
-			ListSelectionListener {
-
-		private GradientEditor editor;
-
-		private JColorChooser chooser;
-
-		public ColorAdaptor(GradientEditor editor, JColorChooser chooser) {
-			super();
-			this.editor = editor;
-			this.chooser = chooser;
-
-			editor.getColorSelectionModel().addListSelectionListener(this);
-			chooser.getSelectionModel().addChangeListener(this);
-		}
-
-		public void valueChanged(ListSelectionEvent e) {
-			int index = editor.getColorSelectionModel().getLeadSelectionIndex();
-			// System.out.println(index);
-			// System.out.println(e.getFirstIndex());
-			// System.out.println(e.getLastIndex());
-			// System.out.println("-------");
-
-			Color c = (Color) editor.getDataModel().getValueAt(index,
-					editor.getColorColumn());
-			chooser.setColor(c);
-		}
-
-		public void stateChanged(ChangeEvent e) {
-
-			if (!editor.getColorSelectionModel().isSelectionEmpty()) {
-				int index = editor.getColorSelectionModel()
-						.getLeadSelectionIndex();
-
-				editor.getDataModel().setValueAt(chooser.getColor(), index,
-						editor.getColorColumn());
-			}
-
-		}
-
-	}
-
 }
